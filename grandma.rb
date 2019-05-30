867255ba42c27120769b1cb124b417a5109b64cf
Ana Pichardo <kayablissfilms@gmail.com>
2019-05-30 01:20:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ if phrase == phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
-else phrase == "I LOVE YOU GRANDMA"
+elseif phrase == "I LOVE YOU GRANDMA"
    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
