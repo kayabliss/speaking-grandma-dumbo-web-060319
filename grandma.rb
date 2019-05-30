4f501a024d15fedf26f7764ab3fdf7b5fafa74b8
Ana Pichardo <kayablissfilms@gmail.com>
2019-05-30 01:21:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase = "I LOVE YOU GRANDMA")
-if phrase == phrase.downcase 
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == phrase.upcase
-  puts "NO, NOT SINCE 1938!"
-elseif phrase == "I LOVE YOU GRANDMA"
-   puts "I LOVE YOU TOO PUMPKIN!"
+  if phrase == phrase.downcase 
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
