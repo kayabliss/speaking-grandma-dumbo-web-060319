c05269cb906dd19087a29ab7cb94f662db9113b5
Ana Pichardo <kayablissfilms@gmail.com>
2019-05-30 01:46:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
+  if phrase == !phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
